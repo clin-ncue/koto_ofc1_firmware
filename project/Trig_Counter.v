4ef9ef0554c002b5e4339d71ab3978e33b08cb3f
Chieh Lin <chiehlin@uchicago.edu>
2022-03-23 17:13:47 -0500

    v0.04.05 add debug tools, remove energy word check, introduce overflow error.

--- project/Trig_Counter.v
+++ project/Trig_Counter.v
@@ -1,54 +1,42 @@
-module Trig_Counter (
-	clk,
-	rst,
-	L1A,
-	PL1A,
-	PS,
-	ALIGN,
-	DELTA,
-	cnt_L1A,
-	cnt_PL1A,
-	cnt_PS,
-	cnt_ALIGN,
-	cnt_DELTA
+/* 
+   trigger counter
+   C. Lin, chiehlin@uchicago.edu
+*/
+
+module trig_counter
+(
+// input 
+  clk               , // system clock
+  reset             ,
+  
+  // inputs
+  in                , 
+   
+  // output
+  q                       
+              
 );
 
+input wire         clk;
+input wire         reset;
 
-input wire clk;
-input wire rst;
-input wire L1A;
-input wire PL1A;
-input wire PS;
-input wire ALIGN;
-input wire DELTA;
+// inputs
+input wire         in;
 
+// output
+output reg [15: 0] q;
 
-output reg [31:0] cnt_L1A = 32'b0;
-output reg [31:0] cnt_PL1A = 32'b0;
-output reg [31:0] cnt_PS = 32'b0;
-output reg [31:0] cnt_ALIGN = 32'b0;
-output reg [31:0] cnt_DELTA = 32'b0;
+////////////////////////////////////////////
+always @(posedge clk) begin
 
-always @(posedge clk)
-begin
+   if( reset==1'b1 ) begin
+      q <= 0;
+   end
 
-	if(rst)
-	begin	
-		cnt_L1A = 32'b0;
-		cnt_PL1A = 32'b0;
-		cnt_PS = 32'b0;
-		cnt_ALIGN = 32'b0;
-		cnt_DELTA = 32'b0;
-	end
-	
-	cnt_L1A = (L1A==1'b1)? cnt_L1A + 1'b1 : cnt_L1A;
-	cnt_PL1A = (PL1A==1'b1)? cnt_PL1A + 1'b1 : cnt_PL1A;
-	cnt_PS = (PS==1'b1)? cnt_PS + 1'b1 : cnt_PS;
-	cnt_ALIGN = (ALIGN==1'b1)? cnt_ALIGN + 1'b1 : cnt_ALIGN;
-	cnt_DELTA = (DELTA==1'b1)? cnt_DELTA + 1'b1 : cnt_DELTA;
-	
-
-	
+   if( in==1'b1 && q<16'hFFFF ) begin
+      q <= q + 1;
+   end
+		
 end
 
 endmodule
--- project/error_coder.v
+++ project/error_coder.v
@@ -9,6 +9,10 @@
 	- ERROR  | 1st	       | pending, spill no, event no
 	- STOP   | 2nd        | memory almost full
 	- WARNING| 3rd        | energy word error
+	
+	2022.03.22
+	- Remove energy word error coding.
+	- Add prioritrization logic: 
 */
 
 module error_coder
@@ -18,12 +22,13 @@ module error_coder
   clk               , // system clock
   
   // inputs
+  live_rising       ,
   pending_err       ,
   evtno_err         ,
   spillno_err       ,
-  eneword_err       ,
   stop_rising       ,
   stop_falling      , 
+  read_overflow     ,
 	
   // outputs
   err_out  
@@ -33,12 +38,13 @@ module error_coder
 input wire         clk;
 
 // inputs
+input wire         live_rising;
 input wire         pending_err;
 input wire         evtno_err;
 input wire         spillno_err;
-input wire         eneword_err;
 input wire         stop_rising;
 input wire         stop_falling;
+input wire         read_overflow;
 
 // output
 output reg         err_out;
@@ -47,39 +53,56 @@ output reg         err_out;
 reg        [1  :0] level;
 reg                lock;
 reg        [2  :0] cnt;
+reg                err_sent = 1'b0;
+
+wire err;
+assign err = ( pending_err || evtno_err || spillno_err || read_overflow );
 
 ////////////////////////////////////////////
 always @(posedge clk) begin
 
-   /// receives errors 
-	if( pending_err == 1'b1 || evtno_err == 1'b1 || spillno_err == 1'b1 ) begin
-	    level <= 0;
-		cnt <= 0;
-		lock <= 1'b1;
-	end
-	else if( stop_rising == 1'b1 || stop_falling == 1'b1 ) begin
-	    level <= 1;
-		cnt <= 0;
-		lock <= 1'b1;
-	end
-	else if( eneword_err == 1'b1 ) begin
-	    level <= 2;
-		cnt <= 0;
-		lock <= 1'b1;
-	end
+    // Reset
+    if( live_rising == 1'b1 ) begin
+        err_sent <= 1'b0;
+        lock <= 1'b0;
+        cnt <= 0;
+    end
 
-	if( cnt == ERROR_LENGTH ) begin
+   /// error output logic
+   if( cnt == ERROR_LENGTH ) begin
 	   lock <= 1'b0;
 	   err_out <= 1'b0;
+		cnt <= 0;
 	end 
 	else if( lock == 1'b1 ) begin
-	   err_out <= ( cnt <= level ) ? 1'b1 : 1'b0;
+	   err_out <= ( cnt < level ) ? 1'b1 : 1'b0;
 	   cnt <= cnt + 1;
 	end
 	else begin
 	   err_out <= 1'b0;
-	end
-   
+   end
+
+	 //
+    // Send error / stop if it is not locked and no error is sent in this spill.
+    // 
+    // 1.   "err" signal keeps 1'b1 during the entire spill.
+    //      If the output is locked due to STOP, ERROR is pending.
+    // 
+    // 2.   ERROR has a higher priority than STOP.
+    //      If the output is locked due to ERROR, the STOP signal will NOT be sent.
+    //      However, it is okay because no trigger will be issued toward ERROR.
+    //
+    if( lock == 1'b0 && err_sent == 1'b0 && err == 1'b1 ) begin
+        lock <= 1'b1;
+        err_sent <= 1'b1;
+        level <= 1;
+        cnt <= 0;
+    end
+    else if( lock == 1'b0 && (stop_rising==1'b1 || stop_falling==1'b1) ) begin
+        lock <= 1'b1;
+        level <= 2;
+        cnt <= 0;
+    end
 	
 end
 
