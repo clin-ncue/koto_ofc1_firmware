6f059a66bc4f252081f223d2965bf75d2323653c
Chieh Lin <chiehlin@UCHICAGO.EDU>
2024-01-04 14:25:07 -0600

    v1.01.00 introduce ping-pong logic at upstream (not finished yet)
    - If a package is identified by "header identifier", a "get" signal will
      be sent to A and B alternately. This is reset by the rising edge of
      LIVE.

---
+++ project/ping_pong.v
@@ -0,0 +1,50 @@
+/* 
+   ping_pong
+   C. Lin, chiehlin@uchicago.edu
+	
+*/
+
+module ping_pong
+(
+// input 
+  clk               , // system clock
+  
+  // inputs
+  reset             ,
+  in                ,
+   
+  // output
+  outA              ,
+  outB             
+);
+
+input wire         clk;
+input wire         reset;
+
+// inputs
+input wire         in;
+
+// output
+output reg         outA;
+output reg         outB;
+
+reg                is_A_B;
+
+////////////////////////////////////////////
+always @(posedge clk) begin
+
+   /// reset
+	if( reset==1'b1 ) begin
+	   is_A_B <= 1'b0;
+	end
+
+	outA <= (is_A_B==1'b0) ? in : 1'b0;  
+	outB <= (is_A_B==1'b1) ? in : 1'b0;
+	
+	is_A_B <= (in==1'b1) ? (!is_A_B) : is_A_B;
+	
+	
+
+end
+
+endmodule
